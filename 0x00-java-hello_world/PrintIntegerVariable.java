d0ce99b73c14e586a24b491cb7c1fb676788102b
Litoxam <litobernier@gmail.com>
2026-06-16 17:33:50 +0200

    fix: coma added

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -3,6 +3,6 @@ public class PrintIntegerVariable {
     public static void main(String[] args) {
 
         int number = 1600;
-        System.out.println("Pennsylvania Avenue " + number);
+        System.out.println("Pennsylvania Avenue, " + number);
     }
 }
