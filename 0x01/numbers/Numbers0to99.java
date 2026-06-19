d023550e057511d26e416eaa08f28d1e943efbc6
Litoxam <litobernier@gmail.com>
2026-06-19 17:03:39 +0200

    Prints numbers 0 to 99

---
+++ 0x01/numbers/Numbers0to99.java
@@ -0,0 +1,10 @@
+public class Numbers0to99 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 99; i++){
+            if (i < 99)
+            System.out.printf(i + ", ");
+            else
+            System.out.printf(i + "\n");
+        }  
+    }
+}
