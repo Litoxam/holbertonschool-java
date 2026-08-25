29725f02c7b4f9f78fb6c6c60dba97ff241c356f
Litoxam <litobernier@gmail.com>
2026-08-25 16:50:04 +0200

    Character Status

---
+++ Classes_Objects/status/CharacterGame.java
@@ -0,0 +1,47 @@
+public class CharacterGame {
+    private int currentHealth;
+    private String name;
+    private String status;
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+        if (currentHealth > 0) {
+            this.status = "alive";
+        }
+        else {
+            this.status = "dead";
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void takeDamage(int damageAmount) {
+        int newHealth = currentHealth - damageAmount;
+        if (newHealth < 0) {
+            newHealth = 0;
+        }
+        setCurrentHealth(newHealth);
+    }
+
+    public void receiveHealing(int healingAmount) {
+        int newHealth = currentHealth + healingAmount;
+        if (newHealth > 100) {
+            newHealth = 100;
+        }
+        setCurrentHealth(newHealth);
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
